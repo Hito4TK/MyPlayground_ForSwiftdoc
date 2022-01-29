ef28251b1e53f0e758e470e393b1d2a826f8a830
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-01-29 23:50:39 +0900

    rename files

--- MyPlayground_ForSwiftDoc.playground/Pages/Test.xcplaygroundpage/Contents.swift
+++ MyPlayground_ForSwiftDoc.playground/Pages/Test.xcplaygroundpage/Contents.swift
@@ -3,5 +3,10 @@
 import Foundation
 
 var greeting = "Hello, playground"
+print(greeting)
+
+let hoge = Hoge(text: "aiueo")
+
+print(hoge.okok)
 
 //: [Next](@next)
---
+++ MyPlayground_ForSwiftDoc.playground/Pages/Test.xcplaygroundpage/Sources/Hoge.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct Hoge {
+    public let okok: String
+    
+    public init(text:String) {
+        okok = text
+    }
+}
