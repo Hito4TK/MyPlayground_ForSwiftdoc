e1e1489f30ed027915483a5a887e011f1302c258
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-02-15 00:11:21 +0900

    ended Error Handling

--- MyPlayground_ForSwiftDoc.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ MyPlayground_ForSwiftDoc.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -10,7 +10,156 @@ enum VendingMachineError: Error {
     case outOfStock
 }
 
-throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+//throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
 
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    var coinsDeposited = 0
+
+    func vend(itemNamed name: String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+
+        guard item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+
+        guard item.price <= coinsDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+        }
+
+        coinsDeposited -= item.price
+
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+
+        print("Dispensing \(name)")
+    }
+}
+
+let favoriteSnacks = [
+    "Alice": "Chips",
+    "Bob": "Licorice",
+    "Eve": "Pretzels",
+]
+func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+    let snackName = favoriteSnacks[person] ?? "Candy Bar"
+    try vendingMachine.vend(itemNamed: snackName)
+}
+struct PurchasedSnack {
+    let name: String
+    init(name: String, vendingMachine: VendingMachine) throws {
+        try vendingMachine.vend(itemNamed: name)
+        self.name = name
+    }
+}
+
+var vendingMachine = VendingMachine()
+vendingMachine.coinsDeposited = 8
+do {
+    try buyFavoriteSnack(person: "Alice", vendingMachine: vendingMachine)
+    print("Success! Yum.")
+} catch VendingMachineError.invalidSelection {
+    print("Invalid Selection.")
+} catch VendingMachineError.outOfStock {
+    print("Out of Stock.")
+} catch VendingMachineError.insufficientFunds(let coinsNeeded) {
+    print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+} catch {
+    print("Unexpected error: \(error).")
+}
+
+func nourish(with item: String) throws {
+    do {
+        try vendingMachine.vend(itemNamed: item)
+    } catch is VendingMachineError {
+        print("Couldn't buy that from the vending machine.")
+    }
+}
+
+do {
+    try nourish(with: "Beet-Flavored Chips")
+} catch {
+    print("Unexpected non-vending-machine-related error: \(error)")
+}
+
+func eat(item: String) throws {
+    do {
+        try vendingMachine.vend(itemNamed: item)
+    } catch VendingMachineError.invalidSelection, VendingMachineError.insufficientFunds, VendingMachineError.outOfStock {
+        print("Invalid selection, out of stock, or not enough money.")
+    }
+}
+
+do {
+    try eat(item: "hoge")
+} catch {
+    print("general error : \(error)")
+}
+
+func someThrowingFunction() throws -> Int {
+    let dict = [
+        "hoge":123,
+        "puge":456
+    ]
+    guard let val = dict["bobu"] else {
+        throw VendingMachineError.outOfStock
+    }
+    
+    return val
+    // ...
+}
+
+let x = try? someThrowingFunction()
+
+let y: Int?
+do {
+    y = try someThrowingFunction()
+} catch {
+    y = nil
+}
+print(x ?? "hoge",y ?? "buge")
+
+
+//let photo = try! loadImage(atPath: "./Resources/John Appleseed.jpg")
+
+/*
+func processFile(filename: String) throws {
+    if exists(filename) {
+        let file = open(filename)
+        defer {
+            close(file)
+        }
+        while let line = try file.readline() {
+            // Work with the file.
+        }
+        // close(file) is called here, at the end of the scope.
+    }
+}
+*/
+
+func someFunc() -> String {
+    let a = "hoge"
+    defer {
+        print("last")
+    }
+    defer {
+        print("last2")
+    }
+    return a
+}
+
+print(someFunc())
 
 //: [Next](@next)
