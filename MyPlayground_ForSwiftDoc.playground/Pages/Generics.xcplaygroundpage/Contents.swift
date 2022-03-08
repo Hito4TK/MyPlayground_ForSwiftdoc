ca35a53dcff934a5523964b13800707d054c6dad
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-03-08 22:03:07 +0900

    ended Generics

--- MyPlayground_ForSwiftDoc.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ MyPlayground_ForSwiftDoc.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -52,7 +52,7 @@ struct IntStack {
         return items.removeLast()
     }
 }
-struct Stack<Element> {
+struct StackOne<Element> {
     var items: [Element] = []
     mutating func push(_ item: Element) {
         items.append(item)
@@ -61,7 +61,7 @@ struct Stack<Element> {
         return items.removeLast()
     }
 }
-var stackOfStrings = Stack<String>()
+var stackOfStrings = StackOne<String>()
 stackOfStrings.push("uno")
 stackOfStrings.push("dos")
 stackOfStrings.push("tres")
@@ -69,7 +69,7 @@ stackOfStrings.push("cuatro")
 let fromTheTop = stackOfStrings.pop()
 print(fromTheTop)
 
-extension Stack {
+extension StackOne {
     var topItem: Element? {
         return items.isEmpty ? nil : items[items.count - 1]
     }
@@ -112,6 +112,292 @@ func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
 let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
 let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
 
+
+
+protocol Container {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+
+struct Hoge: Container {
+    var items: [Item] = []
+    
+    typealias Item = String
+    mutating func append(_ item: Item) {
+        self.items.append(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Item {
+        return items[i]
+    }
+}
+
+var hogege = Hoge()
+hogege.append("buge")
+hogege.append("noneno")
+print(hogege.count)
+
+struct IntStackUsingContainer: Container {
+    // original IntStack implementation
+    var items: [Item] = []
+    mutating func push(_ item: Item) {
+        items.append(item)
+    }
+    mutating func pop() -> Item {
+        return items.removeLast()
+    }
+    // conformance to the Container protocol
+    typealias Item = Bool
+    mutating func append(_ item: Item) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Item {
+        return items[i]
+    }
+}
+
+var stackOfStringsUsingContainer = IntStackUsingContainer()
+stackOfStringsUsingContainer.push(true)
+stackOfStringsUsingContainer.push(false)
+stackOfStringsUsingContainer.push(false)
+stackOfStringsUsingContainer.push(true)
+let fromTheTopaaa = stackOfStringsUsingContainer.pop()
+print(fromTheTopaaa)
+
+
+struct Stack<Element>: Container {
+    // original Stack<Element> implementation
+    var items: [Element] = []
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    // conformance to the Container protocol
+    mutating func append(_ item: Element) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
+
+extension Array: Container {}
+
+protocol ContainerEq {
+    associatedtype Item: Equatable
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+struct Boge: ContainerEq {
+    typealias Item = String
+    
+    var items:[Item] = []
+    mutating func append(_ item: String) {
+        self.items.append(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> String {
+        return items[i]
+    }
+}
+var boge = Boge()
+boge.append("aiu")
+boge.append("eo")
+var boge2 = Boge()
+boge2.append("aiu")
+boge2.append("eo")
+print(boge[0])
+
+print(boge[0] == boge2[0])
+
+
+protocol SuffixableContainer: Container {
+    associatedtype Suffix: SuffixableContainer where Suffix.Item == Item
+    func suffix(_ size: Int) -> Suffix
+}
+
+extension Stack: SuffixableContainer {
+    func suffix(_ size: Int) -> Stack {
+        var result = Stack()
+        for index in (count-size)..<count {
+            result.append(self[index])
+        }
+        return result
+    }
+    // Inferred that Suffix is Stack.
+}
+var stackOfInts = Stack<Int>()
+stackOfInts.append(10)
+stackOfInts.append(20)
+stackOfInts.append(30)
+let suffix = stackOfInts.suffix(2)
+print(suffix)
+
+/*
+extension IntStack: SuffixableContainer {
+    func suffix(_ size: Int) -> Stack<Int> {
+        var result = Stack<Int>()
+        for index in (count-size)..<count {
+            result.append(self[index])
+        }
+        return result
+    }
+    // Inferred that Suffix is Stack<Int>.
+}
+*/
+
+func allItemsMatch<C1: Container, C2: Container>
+    (_ someContainer: C1, _ anotherContainer: C2) -> Bool
+    where C1.Item == C2.Item, C1.Item: Equatable {
+
+        // Check that both containers contain the same number of items.
+        if someContainer.count != anotherContainer.count {
+            return false
+        }
+
+        // Check each pair of items to see if they're equivalent.
+        for i in 0..<someContainer.count {
+            if someContainer[i] != anotherContainer[i] {
+                return false
+            }
+        }
+
+        // All items match, so return true.
+        return true
+}
+
+var stackOfStrings2 = Stack<String>()
+stackOfStrings2.push("uno")
+stackOfStrings2.push("dos")
+stackOfStrings2.push("tres")
+
+var arrayOfStrings = ["uno", "dos", "tres"]
+
+if allItemsMatch(stackOfStrings2, arrayOfStrings) {
+    print("All items match.")
+} else {
+    print("Not all items match.")
+}
+
+extension Stack where Element: Equatable {
+    func isTop(_ item: Element) -> Bool {
+        guard let topItem = items.last else {
+            return false
+        }
+        return topItem == item
+    }
+}
+
+if stackOfStrings2.isTop("tres") {
+    print("Top element is tres.")
+} else {
+    print("Top element is something else.")
+}
+
+/*
+struct NotEquatable { }
+var notEquatableStack = Stack<NotEquatable>()
+let notEquatableValue = NotEquatable()
+notEquatableStack.push(notEquatableValue)
+notEquatableStack.isTop(notEquatableValue)
+*/
+
+extension Container where Item: Equatable {
+    func startsWith(_ item: Item) -> Bool {
+        return count >= 1 && self[0] == item
+    }
+}
+
+if [9, 9, 9].startsWith(42) {
+    print("Starts with 42.")
+} else {
+    print("Starts with something else.")
+}
+
+extension Container where Item == Double {
+    func average() -> Double {
+        var sum = 0.0
+        for index in 0..<count {
+            sum += self[index]
+        }
+        return sum / Double(count)
+    }
+}
+print([1260.0, 1200.0, 98.6, 37.0].average())
+
+extension Container {
+    func average() -> Double where Item == Int {
+        var sum = 0.0
+        for index in 0..<count {
+            sum += Double(self[index])
+        }
+        return sum / Double(count)
+    }
+    func endsWith(_ item: Item) -> Bool where Item: Equatable {
+        return count >= 1 && self[count-1] == item
+    }
+}
+let numbers = [1260, 1200, 98, 37]
+print(numbers.average())
+print(numbers.endsWith(37))
+
+/*
+extension Container where Item == Int {
+    func average() -> Double {
+        var sum = 0.0
+        for index in 0..<count {
+            sum += Double(self[index])
+        }
+        return sum / Double(count)
+    }
+}
+extension Container where Item: Equatable {
+    func endsWith(_ item: Item) -> Bool {
+        return count >= 1 && self[count-1] == item
+    }
+}
+*/
+
+
+protocol Container_AssociatedTypesWithGenericWhereClause {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+
+    associatedtype Iterator: IteratorProtocol where Iterator.Element == Item
+    func makeIterator() -> Iterator
+}
+
+protocol ComparableContainer: Container_AssociatedTypesWithGenericWhereClause where Item: Comparable { }
+
+extension Container {
+    subscript<Indices: Sequence>(indices: Indices) -> [Item]
+        where Indices.Iterator.Element == Int {
+            var result: [Item] = []
+            for index in indices {
+                result.append(self[index])
+            }
+            return result
+    }
+}
 
 
 //: [Next](@next)
