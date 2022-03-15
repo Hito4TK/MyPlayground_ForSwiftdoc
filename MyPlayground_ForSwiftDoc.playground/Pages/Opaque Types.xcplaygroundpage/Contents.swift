95249475af6a4e8b6eb1fce4e3603336eb0aaf7c
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-03-15 23:09:09 +0900

    studied Opaque Types

--- MyPlayground_ForSwiftDoc.playground/Pages/Opaque Types.xcplaygroundpage/Contents.swift
+++ MyPlayground_ForSwiftDoc.playground/Pages/Opaque Types.xcplaygroundpage/Contents.swift
@@ -117,4 +117,44 @@ for val in hoge {
 }
 
 
+func protoFlip<T: Shape>(_ shape: T) -> Shape {
+    if shape is Square {
+        return shape
+    }
+
+    return FlippedShape(shape: shape)
+}
+
+let protoFlippedTriangle = protoFlip(smallTriangle)
+let sameThing = protoFlip(smallTriangle)
+//protoFlippedTriangle == sameThing  // Error
+
+protocol Container {
+    associatedtype Item
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+extension Array: Container { }
+
+
+/*
+// Error: Protocol with associated types can't be used as a return type.
+func makeProtocolContainer<T>(item: T) -> Container {
+    return [item]
+}
+
+// Error: Not enough information to infer C.
+func makeProtocolContainer<T, C: Container>(item: T) -> C {
+    return [item]
+}
+*/
+
+func makeOpaqueContainer<T>(item: T) -> some Container {
+    return [item]
+}
+let opaqueContainer = makeOpaqueContainer(item: 12)
+let twelve = opaqueContainer[0]
+print(type(of: twelve))
+
+
 //: [Next](@next)
